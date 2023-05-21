ff2c1c11c7cfd7c8e6c3bb3a2e9fee8d720d5225
nursich <nurlibaiem04@mail.ru>
2023-05-21 18:51:35 +0600

    Implements method 'delete' and 'findMin'

--- src/BST.java
+++ src/BST.java
@@ -50,11 +50,41 @@ public class BST<K extends Comparable<K>, V> {
     }
 
     public void delete(K key) {
+        root = delete(root, key);
+    }
 
+    public Node delete(Node node, K key) {
+        if (node == null) {
+            return null;
+        }
+        int cmp = key.compareTo(node.key);
+        if (cmp < 0)
+            node.left = delete(node.left, key);
+        else if (cmp > 0)
+            node.right = delete(node.right, key);
+        else {
+            if (node.left == null) {
+                return node.right;
+            } else if (node.right == null) {
+                return node.left;
+            } else {
+                Node minRightNode = findMin(node.right);
+                node.key = minRightNode.key;
+                node.val = minRightNode.val;
+                node.right = delete(node.right, minRightNode.key);
+            }
+        }
+        return node;
+    }
+
+    public Node findMin(Node node) {
+        while (node.left != null) {
+            node = node.left;
+        }
+        return node;
     }
 
     public Iterable<K> iterator() {
         return null;
     }
-
 }
